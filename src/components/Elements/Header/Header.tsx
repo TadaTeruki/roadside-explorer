22179aabc1475d4f20e01a37544d5518c10cec13
Teruki TADA <tadateruki.public@gmail.com>
2023-09-08 01:14:40 +0900

    [fix] format

--- src/components/Elements/Header/Header.tsx
+++ src/components/Elements/Header/Header.tsx
@@ -4,7 +4,9 @@ export const Header = () => {
   return (
     <header className={styles.header}>
       <div className={styles.title}>Roadside Explorer</div>
-	  <div className={styles.subtitle}>想い尽きるまでロードサイドを眺めていたい</div>
+      <div className={styles.subtitle}>
+        想い尽きるまでロードサイドを眺めていたい
+      </div>
     </header>
   );
 };
